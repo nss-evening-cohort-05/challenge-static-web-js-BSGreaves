19a0cd6a221c61ec4487274cdf12e1a73a2313c3
Ben Greaves <bsgreaves@gmail.com>
2017-02-11 11:52:24 -0600

    Doh. I left commented code in there! Deleted it.

--- tree.js
+++ tree.js
@@ -6,26 +6,6 @@ var userCharacter = document.getElementById("userCharacter");
 
 // FUNCTIONS
 
-// Technically, growTree only takes a single object as its parameter.
-// Of course, it relies on the defineTree function to assemble and validate the object.
-// Hopefully that doesn't break the spirit of the assignment.
-// function growTree(inputObject) {
-// 	let character = inputObject.character;
-// 	let space = " ";
-// 	let targetWidth = (inputObject.height * 2) - 1;
-// 	let treeSpaces = 1;
-// 	let openSpaces = (targetWidth - 1) / 2;
-// 	while (treeSpaces <= targetWidth) {
-// 		let currTreeOutput = "";
-// 		let currSpace = space.repeat(openSpaces);
-// 		let currTree = character.repeat(treeSpaces);
-// 		currTreeOutput = currSpace + currTree + currSpace;
-// 		console.log(currTreeOutput);
-// 		treeSpaces += 2;
-// 		openSpaces -= 1;
-// 	}
-// };
-
 function growTree(inputObject) {
 	let character = inputObject.character;
 	let space = " ";
