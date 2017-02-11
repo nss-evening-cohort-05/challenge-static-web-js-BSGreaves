1ec8a1c6fbc9bbf89749ee50f0ebb6ace0125f5a
Ben Greaves <bsgreaves@gmail.com>
2017-02-11 11:50:17 -0600

    Minor changes. Added line breaks to console.log. Moved the console.log so that it outputs onces. Improved number validation to check whether the input was actually a number.

--- tree.js
+++ tree.js
@@ -9,29 +9,47 @@ var userCharacter = document.getElementById("userCharacter");
 // Technically, growTree only takes a single object as its parameter.
 // Of course, it relies on the defineTree function to assemble and validate the object.
 // Hopefully that doesn't break the spirit of the assignment.
+// function growTree(inputObject) {
+// 	let character = inputObject.character;
+// 	let space = " ";
+// 	let targetWidth = (inputObject.height * 2) - 1;
+// 	let treeSpaces = 1;
+// 	let openSpaces = (targetWidth - 1) / 2;
+// 	while (treeSpaces <= targetWidth) {
+// 		let currTreeOutput = "";
+// 		let currSpace = space.repeat(openSpaces);
+// 		let currTree = character.repeat(treeSpaces);
+// 		currTreeOutput = currSpace + currTree + currSpace;
+// 		console.log(currTreeOutput);
+// 		treeSpaces += 2;
+// 		openSpaces -= 1;
+// 	}
+// };
+
 function growTree(inputObject) {
 	let character = inputObject.character;
 	let space = " ";
 	let targetWidth = (inputObject.height * 2) - 1;
 	let treeSpaces = 1;
 	let openSpaces = (targetWidth - 1) / 2;
+	let outputToConsole = ""
 	while (treeSpaces <= targetWidth) {
-		let currTreeOutput = "";
 		let currSpace = space.repeat(openSpaces);
 		let currTree = character.repeat(treeSpaces);
-		currTreeOutput = currSpace + currTree + currSpace;
-		console.log(currTreeOutput);
+		outputToConsole += currSpace + currTree + currSpace + "\n";
 		treeSpaces += 2;
 		openSpaces -= 1;
 	}
+	console.log(outputToConsole);
 };
+
 
 function defineTree() {
 	let userInput = {};
 	userInput.height = document.getElementById("userHeight").value;
 	userInput.character = document.getElementById("userCharacter").value;
-	if (userInput.height === "" || userInput.character === "") {
-		alert("Please enter a value into both fields.");
+	if (userInput.height === "" || userInput.character === "" || isNaN(userInput.height) === true) {
+		alert("Please enter a valid number into the first field and a character into the second field.");
 	} else {
 	growTree(userInput);
 	}
